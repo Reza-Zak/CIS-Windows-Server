3d30aefc9ba617f898574bd640ebf23195807bf7
Reza-Zak <59948055+Reza-Zak@users.noreply.github.com>
2025-11-09 08:16:25 +1100

    Implement dynamic loading of module functions
    Dynamically load and export public and private functions.

--- Modules/BaselineUtils/BaselineUtils.psm1
+++ Modules/BaselineUtils/BaselineUtils.psm1
@@ -1,1 +1,13 @@
+# Load Public and Private functions dynamically
+$public  = Join-Path $PSScriptRoot 'Public'
+$private = Join-Path $PSScriptRoot 'Private'
+
+# Dot-source all function files
+Get-ChildItem -Path $public -Filter *.ps1 | ForEach-Object { . $_.FullName }
+Get-ChildItem -Path $private -Filter *.ps1 | ForEach-Object { . $_.FullName }
+
+# Export only Public functions
+Export-ModuleMember -Function (Get-ChildItem -Path $public -Filter *.ps1 | ForEach-Object {
+    (Get-Command -Path $_.FullName).Name
+})
 
