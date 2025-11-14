6c3a8fbf2186046bdccfae8f10cfb9516c6b2856
Reza-Zak <59948055+Reza-Zak@users.noreply.github.com>
2025-11-14 17:26:48 +1100

    Update CIS_2.3.1.2_LimitBlankPasswords.Tests.ps1

--- CIS-Checks/2_Local_Policies/2.3_Security_Options/2.3.1_Accounts/CIS_2.3.1.2_LimitBlankPasswords.Tests.ps1
+++ CIS-Checks/2_Local_Policies/2.3_Security_Options/2.3.1_Accounts/CIS_2.3.1.2_LimitBlankPasswords.Tests.ps1
@@ -1,41 +1,37 @@
 <#
 .SYNOPSIS
-    CIS Check: 2.3.1.2 - Limit local account use of blank passwords to console logon only
+    CIS Check: 2.3.1.2 - Accounts: Limit local account use of blank passwords to console logon only
 
 .DESCRIPTION
-    Ensures the setting:
-        'LimitBlankPasswordUse'
-    is ENABLED (1).
-
-    Value meanings:
-        0 = Disabled  (NON-compliant)
-        1 = Enabled   (CIS compliant)
+    Ensures blank passwords can only be used for console logons.
+    CIS requirement: Value must be 1 (Enabled).
 
 .NOTES
-    Layer: 2
     CIS Level: 1
-    Applies to all servers
+    Automated Check
+
+.LINK
+    https://www.cisecurity.org/benchmark/windows_server
 #>
 
 param()
 
-$script:testTags = @('Level1','Server2019','Server2022','Enabled')
+$script:testTags = @("Level1","Server2019","Server2022","Enabled")
 
 BeforeAll {
 
-    Import-Module BaselineUtils -ErrorAction Stop
-    Test-AdminRights
+    Test-AdministratorRights
 
-    $script:optionValue = Get-SecurityOptionValue -OptionName 'LimitBlankPasswordUse'
+    # Retrieve registry-based Security Option
+    $script:blankPasswordSetting = Get-SecurityOptionValue -OptionName "LimitBlankPasswordUse"
 }
 
-Describe "CIS Check: 2.3.1.2 - Limit blank password use to console logon only" -Tag $script:testTags {
+Describe "CIS 2.3.1.2 - Limit blank passwords to console logon only" -Tag $script:testTags {
 
-    Context "Security Option: LimitBlankPasswordUse" {
+    Context "Security option: LimitBlankPasswordUse" {
 
-        It "Must be enabled (1)" {
-
-            [int]$script:optionValue | Should -Be 1
+        It "Should be Enabled (1)" {
+            [int]$script:blankPasswordSetting | Should -Be 1
         }
     }
 }
